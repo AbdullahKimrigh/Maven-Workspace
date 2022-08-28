7905a5fd144440923d4eacabefa64a78ff7ec11b
AbdullahKimrigh <abdullahkimregh@gmail.com>
2022-08-28 12:41:56 +0300

    update

--- LearningMaven/src/main/java/com/codingdojo/LearningMaven/LearningMavenApplication.java
+++ LearningMaven/src/main/java/com/codingdojo/LearningMaven/LearningMavenApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningMavenApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(LearningMavenApplication.class, args);
 	}
 
