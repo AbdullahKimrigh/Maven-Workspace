252cde1a46e249c8bc37f37eefd4c2a2079b4f0f
Abdullah Kimrigh <48530449+AbdullahKimrigh@users.noreply.github.com>
2022-08-28 11:44:22 +0300

    Add files via upload

---
+++ LearningMaven/src/main/java/com/codingdojo/LearningMaven/LearningMavenApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.LearningMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMavenApplication.class, args);
+	}
+
+}
