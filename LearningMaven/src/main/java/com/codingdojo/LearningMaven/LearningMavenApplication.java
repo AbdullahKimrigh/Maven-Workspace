2a0994eb5afaaf8ccdcd08eb0734275c03eca6ac
AbdullahKimrigh <abdullahkimregh@gmail.com>
2022-08-28 12:47:16 +0300

    update

--- LearningMaven/src/main/java/com/codingdojo/LearningMaven/LearningMavenApplication.java
+++ LearningMaven/src/main/java/com/codingdojo/LearningMaven/LearningMavenApplication.java
@@ -2,13 +2,20 @@ package com.codingdojo.LearningMaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LearningMavenApplication {
+@RestController
+public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		SpringApplication.run(LearningMavenApplication.class, args);
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
